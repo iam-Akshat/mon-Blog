8a839a53f61a024eee2b6840dd16684c062cfd80
Akshat Sethi <masterakshata@gmail.com>
2020-01-09 18:25:31 +0530

    forgot to save app.js

--- app.js
+++ app.js
@@ -85,6 +85,6 @@ app.post('/logmein',function(req,res){
     }
   })
 })
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
   console.log("Server started on port 3000");
 });
