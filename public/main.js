71860cfa24c8f7784192abdbbeec49cbc5922127
Akshat Sethi <masterakshata@gmail.com>
2020-03-08 21:41:46 +0530

    workable navbar

---
+++ public/main.js
@@ -0,0 +1,20 @@
+document.querySelector(".menu-toggle").addEventListener('click',function(){
+    if(document.getElementById("menu").classList.contains('toggled')){
+     
+        document.getElementById("nav").style.width="";
+        document.querySelector(".nav-links").style.height="0";
+        document.querySelector(".nav-links").style.visibility="hidden";
+
+        document.getElementById("menu").classList.remove("toggled");
+        document.querySelector(".nav-links").style.transition="ease-in 0.27s";
+
+    }else{
+       
+        document.getElementById("nav").style.width="50%"; 
+        document.querySelector(".nav-links").style.height="86.5vh";
+        document.querySelector(".nav-links").style.visibility="visible";    
+        document.querySelector(".nav-links").style.transition="ease-in 0.27s";
+        document.getElementById("menu").classList.add("toggled");
+    }
+    
+});
