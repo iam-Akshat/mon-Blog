0887c4d21e277d62667685d410d20e286b469e31
Akshat Sethi <masterakshata@gmail.com>
2020-03-13 13:09:07 +0530

    nav-menu is working but with extra whitespace when it opens

--- public/main.js
+++ public/main.js
@@ -6,13 +6,13 @@ document.querySelector(".menu-toggle").addEventListener('click',function(){
         document.querySelector(".nav-links").style.visibility="hidden";
 
         document.getElementById("menu").classList.remove("toggled");
-        document.querySelector(".nav-links").style.transition="ease-in 0.27s";
+        document.querySelector(".nav-links").style.transition="ease-in-out 0.15s";
 
     }else{
         document.getElementById("nav").style.width="50%"; 
         document.querySelector(".nav-links").style.height="86.5vh";
         document.querySelector(".nav-links").style.visibility="visible";    
-        document.querySelector(".nav-links").style.transition="ease-in 0.27s";
+        document.querySelector(".nav-links").style.transition="ease-in-out 0.15s";
         document.getElementById("menu").classList.add("toggled");
     }
     
