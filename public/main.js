3a917621e2493bd044008f547919f09f245c0924
Akshat Sethi <masterakshata@gmail.com>
2020-05-03 02:35:06 +0530

    added extra files

--- public/main.js
+++ public/main.js
@@ -1,13 +1,18 @@
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
 document.getElementsByClassName('menu-toggle')[0].addEventListener('click',()=>{
     console.log("wtf");
     
     var m=document.getElementsByTagName('nav')[0];
     if((m.style.right=="0px")){
-        m.style.display="none";
         m.style.right="-500px";
+        m.style.width="0";
+        m.style.display="none";
     }else{
         m.style.right="0px";
         m.style.display="block";
+        m.style.width="60%";
     }
     
 });
